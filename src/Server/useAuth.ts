35ad3d5ed8d28df71d37f40010085a7489e5517b
searchme86 <searchme86@gmail.com>
2022-02-11 00:13:36 +0900

    chore: Auth file added

--- src/Server/useAuth.ts
+++ src/Server/useAuth.ts
@@ -1,17 +1,45 @@
 import { getAuth, onAuthStateChanged, User } from '@firebase/auth';
 import { app } from '../Server/FirebaseConfig';
 import { useEffect, useRef, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+export const defaultHeaders: any = {
+  'Content-Type': 'application/json',
+  Accept: 'application/json',
+};
 
 const useAuth = () => {
+  const navigate = useNavigate();
   const auth = getAuth(app);
   const [user, setUser] = useState<User | null>(null);
   let mounted = useRef<boolean>(false);
+
   useEffect(() => {
     mounted.current = true;
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
+    const unsubscribe = onAuthStateChanged(auth, async (user) => {
       console.log(user);
       if (user) {
         if (mounted.current) setUser(user);
+        try {
+          const token = await user.getIdToken();
+          defaultHeaders.Authorization = `Bearer ${token}`;
+
+          // const res = await fetch('/users/me', {
+          //   method: 'GET',
+          //   headers: defaultHeaders,
+          // });
+
+          // const res = await axios.get('/users/me', {
+          //   headers: defaultHeaders,
+          // });
+
+          console.log('tocken', token);
+          console.log(defaultHeaders);
+
+          navigate('/home');
+        } catch (e) {
+          console.log(e);
+        }
       } else {
         if (mounted.current) setUser(null);
       }
@@ -20,7 +48,7 @@ const useAuth = () => {
       mounted.current = false;
       unsubscribe();
     };
-  }, [auth]);
+  }, [auth, navigate]);
 
   return {
     user,
