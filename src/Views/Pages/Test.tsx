deaa26e77518745608221fe90e97079b4ba217d8
searchme86 <searchme86@gmail.com>
2022-02-22 22:01:30 +0900

    chore: auth-stage v1.3

--- src/Views/Pages/Test.tsx
+++ src/Views/Pages/Test.tsx
@@ -1,27 +1,29 @@
-import Form from '../Components/Common/Form';
+// import Form from './Form';
 
-interface lProps {
-  // title: string;
-  // setEmail: () => void;
-  // setPassword: () => void;
-  // handleAction: () => void;
-  title: string;
-  setEmail: (email: string) => void;
-  setPassword: (password: string) => void;
-  handleAction: (id: number) => void;
-}
+// interface lProps {
+//   // title: string;
+//   // setEmail: () => void;
+//   // setPassword: () => void;
+//   // handleAction: () => void;
+//   title: string;
+//   setEmail: (email: string) => void;
+//   setPassword: (password: string) => void;
+//   handleAction: (id: number) => void;
+// }
 
-const Test = ({ title, setEmail, setPassword, handleAction }: lProps) => {
-  return (
-    <div>
-      <Form
-        title={title}
-        setEmail={setEmail}
-        setPassword={setPassword}
-        handleAction={handleAction}
-      />
-    </div>
-  );
-};
+// const Test = ({ title, setEmail, setPassword, handleAction }: lProps) => {
+//   return (
+//     <div>
+//       <Form
+//         title={title}
+//         setEmail={setEmail}
+//         setPassword={setPassword}
+//         handleAction={handleAction}
+//       />
+//     </div>
+//   );
+// };
 
-export default Test;
+// export default Test;
+
+export const test = '2';
